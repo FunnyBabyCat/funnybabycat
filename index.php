ea97daff824c374e3799eb8ca56bb5bb594ae265
FBC <calexdk@gmail.com>
2016-03-09 11:12:05 +0800

    haha

--- index.php
+++ index.php
@@ -161,7 +161,6 @@ class wechatCallbackapiTest
             case "subscribe":
                 $contentStr = "感谢关注 FunnyBabyCat"."\n"."会生活也是很让人羡慕的.";
                 break;
-            
             default:
                 $contentStr = "Unknown Event: ".$object->Event;
                 break;

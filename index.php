6b17468c17b4de27c814ca263937ec3a7d27295c
FBC <calexdk@gmail.com>
2016-03-10 12:52:27 +0800

    test

--- index.php
+++ index.php
@@ -81,14 +81,15 @@ class wechatCallbackapiTest
             $str_key = mb_substr($keyword, 0, -2, "UTF-8");
             if($str == '天气' && !empty($str_key)){
                 $data = weather($str_key);
-                $status = $data->{'HeWeather data service 3.0'}[0]->{'status'};
-                if($status == "ok"){
-                    $contentStr = weather_info($data);
-                }else if($status == "unknown city"){
-                    $contentStr = "未知城市";
-                }else{
-                    $contentStr = "服务器无响应或超时";
-                }
+                $contentStr = weather_info($data);
+                // $status = $data->{'HeWeather data service 3.0'}[0]->{'status'};
+                // if($status == "ok"){
+                //     $contentStr = weather_info($data);
+                // }else if($status == "unknown city"){
+                //     $contentStr = "未知城市";
+                // }else{
+                //     $contentStr = "服务器无响应或超时";
+                // }
             }else if($keyword == "谁是这个世界上最美的人"){
                 $ran = rand(1, 10);
                 switch ($ran) {

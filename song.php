0cee92032475210110012b98ad55bf5ed5bb1769
FBC <calexdk@gmail.com>
2016-03-15 12:32:04 +0800

    test

---
+++ song.php
@@ -0,0 +1,17 @@
+<?php 
+
+	$arr = array(
+		array(
+			'title'=>'My Song 03-15',
+			'description'=>"宇宙中的各位, 请分一些元气给我吧!",
+			'picUrl'=>'https://mmbiz.qlogo.cn/mmbiz/rB6EXcQicm4WPzicwakZ35osylW9cSMlTzoQwfh6Wg6pGlb5NaajozUXE46PruJhwnGTh9NgpD8VKrtCLv4GYk8A/0?wx_fmt=jpeg',
+			'url'=>'http://mp.weixin.qq.com/s?__biz=MzAwMzgyNzI5NA==&mid=402974389&idx=1&sn=b7f7b3f08b5c554631d28be44246ff76#rd',
+		),
+		array(
+			'title'=>'My Song 03-14',
+			'description'=>"Stray dream of you lights the chain of stars to find the way\nIn the darkest night of my lost and frozen heart",
+			'picUrl'=>'https://mmbiz.qlogo.cn/mmbiz/rB6EXcQicm4W1doa8xLnt2zGbx7c6Y9pxn63SkgeKkN5omYHXhX8OYOU0byosGxzmskiajUD9AK6GiaawBdkYqZ3A/0?wx_fmt=jpeg',
+			'url'=>'http://mp.weixin.qq.com/s?__biz=MzAwMzgyNzI5NA==&mid=402960407&idx=1&sn=8dbfb6a175f4fcc56ab53151cab1a645#rd',
+		),
+	);
+ ?>

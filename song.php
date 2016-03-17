37a89fa7d4123eb5f25bebbc9168f55290d99c01
FBC <calexdk@gmail.com>
2016-03-17 09:25:14 +0800

    test

--- song.php
+++ song.php
@@ -2,16 +2,28 @@
 
 	$arr = array(
 		array(
+			'title'=>'My Song 03-17',
+			'description'=>"if I open my heart to you I'm hoping you'll show me what to do",
+			'picUrl'=>'http://mmbiz.qpic.cn/mmbiz/rB6EXcQicm4UsaAZf4iasQoofxxuyd7PHYY1YYufy3aRIKgxgJ7oHICdtgIjbLeDAibNdrOYXlWoEOCPLSDgnBZhg/640?wx_fmt=jpeg&tp=webp&wxfrom=5',
+			'url'=>'http://mp.weixin.qq.com/s?__biz=MzAwMzgyNzI5NA==&mid=403009920&idx=1&sn=1dff96d970498a716446d92307b48c2f#rd',
+		),
+		array(
+			'title'=>'My Song 03-16',
+			'description'=>"Happiness 是 dear sister 的主题曲. ",
+			'picUrl'=>'http://mmbiz.qpic.cn/mmbiz/rB6EXcQicm4UsaAZf4iasQoofxxuyd7PHYO0icPFOrRKx8FP81S7gfYcnlkCdaBsXCx7lzTtq06r3yzicdWRYNBfrA/640?wx_fmt=jpeg&tp=webp&wxfrom=5',
+			'url'=>'http://mp.weixin.qq.com/s?__biz=MzAwMzgyNzI5NA==&mid=403009920&idx=2&sn=aa3007e76b3b567852f374c5de2c8a8e#rd',
+		),
+		array(
 			'title'=>'My Song 03-15',
 			'description'=>"宇宙中的各位, 请分一些元气给我吧!",
-			'picUrl'=>'https://mmbiz.qlogo.cn/mmbiz/rB6EXcQicm4WPzicwakZ35osylW9cSMlTzoQwfh6Wg6pGlb5NaajozUXE46PruJhwnGTh9NgpD8VKrtCLv4GYk8A/0?wx_fmt=jpeg',
-			'url'=>'http://mp.weixin.qq.com/s?__biz=MzAwMzgyNzI5NA==&mid=402974389&idx=1&sn=b7f7b3f08b5c554631d28be44246ff76#rd',
+			'picUrl'=>'http://mmbiz.qpic.cn/mmbiz/rB6EXcQicm4WPzicwakZ35osylW9cSMlTzoQwfh6Wg6pGlb5NaajozUXE46PruJhwnGTh9NgpD8VKrtCLv4GYk8A/640?wx_fmt=jpeg&tp=webp&wxfrom=5',
+			'url'=>'http://mp.weixin.qq.com/s?__biz=MzAwMzgyNzI5NA==&mid=403009920&idx=3&sn=65abcedaae29d33f5f4b55b459bb88cc#rd',
 		),
 		array(
 			'title'=>'My Song 03-14',
 			'description'=>"Stray dream of you lights the chain of stars to find the way\nIn the darkest night of my lost and frozen heart",
-			'picUrl'=>'https://mmbiz.qlogo.cn/mmbiz/rB6EXcQicm4W1doa8xLnt2zGbx7c6Y9pxn63SkgeKkN5omYHXhX8OYOU0byosGxzmskiajUD9AK6GiaawBdkYqZ3A/0?wx_fmt=jpeg',
-			'url'=>'http://mp.weixin.qq.com/s?__biz=MzAwMzgyNzI5NA==&mid=402960407&idx=1&sn=8dbfb6a175f4fcc56ab53151cab1a645#rd',
+			'picUrl'=>'http://mmbiz.qpic.cn/mmbiz/rB6EXcQicm4UsaAZf4iasQoofxxuyd7PHYj7Q5CriaaO2LqSibHFCNJTT8slMRGic7vh0GGkKxI59U1JVLwmZsX9xUw/640?wx_fmt=jpeg&tp=webp&wxfrom=5',
+			'url'=>'http://mp.weixin.qq.com/s?__biz=MzAwMzgyNzI5NA==&mid=403009920&idx=4&sn=8977b29499068dc42d124970d8e69913#rd',
 		),
 	);
  ?>

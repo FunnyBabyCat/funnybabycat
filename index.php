ca6844172b34d591764cfe0e445e209ec4efcfb6
FBC <calexdk@gmail.com>
2016-03-17 10:09:17 +0800

    test

--- index.php
+++ index.php
@@ -79,6 +79,10 @@ class wechatCallbackapiTest
             //天气
             $str = mb_substr($keyword, -2, 2, "UTF-8");
             $str_key = mb_substr($keyword, 0, -2, "UTF-8");
+
+            //翻译
+            $isTrans = mb_substr($keyword, 0, 2, "UTF-8");
+            $trans = mb_substr($keyword, 1, -1, "UTF-8");
             if($str == '天气' && !empty($str_key)){
                 $data = $this->weather($str_key);
                 if($data != null){
@@ -87,6 +91,35 @@ class wechatCallbackapiTest
                     $face = "/::~";
                     $contentStr = $face."发生错误了.../::~";
                 }                
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+            }else if( $isTrans == '翻译' ){
+                $key = "1006358614";
+                $keyfrom = "FunnyBabyCat";
+                $url = "http://fanyi.youdao.com/openapi.do?keyfrom=".$keyfrom."&key=".$key."&type=data&doctype=json&version=1.1&q=".$trans;
+                $trans = json_decode(file_get_contents($url));
+                var_dump($trans);
+                $errorCode = $trans->{"errorCode"};
+                $result = '';
+                switch ($result) {
+                    case 0:
+                        $result = $trans->{"translation"}[0];
+                        break;
+                    case 20:
+                        $result = "要翻译的文本过长";
+                        break;
+                    case 30:
+                        $result = "无法进行有效的翻译";
+                        break;
+                    case 40:
+                        $result = "不支持的语言类型";
+                        break;
+                    case 50:
+                        $result = "无效的key";
+                        break;
+                    default:
+                        $result = '/:,@!出错了...';
+                        break;
+                }
                 $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
             }else if(preg_match("^晚安^", $keyword)){
             	$contentStr = "晚安/:moon";

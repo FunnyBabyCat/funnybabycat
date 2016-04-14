c184559175489595b97c990040b0b871dce65fd8
FBC <calexdk@gmail.com>
2016-04-14 11:14:29 +0800

    test

--- index.php
+++ index.php
@@ -256,6 +256,7 @@ class wechatCallbackapiTest
 
     public function handleVoice($object){
         $contentStr = $pbject->Recognition;
+        $contentStr = "你说的是: ".$contentStr;
         $resultStr = responseText($object, $contentStr);
         return $resultStr;
     }

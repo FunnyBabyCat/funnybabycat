7a06d0c76a58873906d8357a2a31829195d7e6c5
FBC <calexdk@gmail.com>
2016-04-19 11:09:11 +0800

    test

--- index.php
+++ index.php
@@ -104,13 +104,13 @@ class wechatCallbackapiTest
                     $face = "/::~";
                     $contentStr = $face."发生错误了.../::~";
                 }                
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                $resultStr = $this->responseText($postObj, $contentStr);
             }else if( $isTrans == "翻译" ){
                 $contentStr = $this->translate($transinfo);
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                $resultStr = $this->responseText($postObj, $contentStr);
             }else if(preg_match("^晚安^", $keyword)){
             	$contentStr = "晚安/:moon";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                $resultStr = $this->responseText($postObj, $contentStr);
             }else if ( strtolower($keyword) == "song") {
                 include 'song.php';
                 $num = count($arr)>10?10:count($arr);
@@ -149,7 +149,7 @@ class wechatCallbackapiTest
                         $contentStr = "是白雪公主!";
                         break;
                 }
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                $resultStr = $this->responseText($postObj, $contentStr);
             }else if(preg_match("^爱我^", $keyword)){
                 $ran = rand(1, 19);
                 switch ($ran) {
@@ -166,16 +166,16 @@ class wechatCallbackapiTest
                         $contentStr = "哦...";
                         break;
                 }
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                $resultStr = $this->responseText($postObj, $contentStr);
             }else if(preg_match("^嗨|你好|嘿^", $keyword)){
                 $contentStr = "你好, 女神.";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                $resultStr = $this->responseText($postObj, $contentStr);
             }else if(preg_match("^男的|男人|帅哥^", $keyword)){
                 $contentStr = "你好, 丑男.";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                $resultStr = $this->responseText($postObj, $contentStr);
             }else if(preg_match("^[\s\S]*?我[\s\S]*?(丑|不好看|不美|不漂亮)[\s\S]*?^", $keyword)) {
                 $contentStr = "当然不, 每次我看着你的时候, 我都为你的飒爽英姿所倾倒. ";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                $resultStr = $this->responseText($postObj, $contentStr);
             }else if(preg_match("^[\s\S]*?我[\s\S]*?(美|好看|漂亮)[\s\S]*?^", $keyword)) {
                 $ran = rand(1, 2);
                 switch ($ran) {
@@ -186,7 +186,7 @@ class wechatCallbackapiTest
                         $contentStr = "皎若太阳升朝霞, 灼若芙蕖出渌波. ";
                         break;
                 }
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                $resultStr = $this->responseText($postObj, $contentStr);
             }else if(preg_match("^小猫|娘口|在吗|在干嘛^", $keyword)){
                 $ran = rand(1, 2);
                 switch ($ran) {
@@ -197,7 +197,7 @@ class wechatCallbackapiTest
                         $contentStr = "干嘛";
                         break;
                 }
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                $resultStr = $this->responseText($postObj, $contentStr);
             }else{
                 $ran = rand(1, 4);
                 switch ($ran) {
